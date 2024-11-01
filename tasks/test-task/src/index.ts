c191d3c7ce7310251366a888c658a12a1c6c9ea4
BernardoFuret <bernardo.furet@hotmail.com>
2024-11-01 23:08:21 +0000

    logger: User logger lib in example

--- tasks/test-task/src/index.ts
+++ tasks/test-task/src/index.ts
@@ -1,3 +1,15 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import Logger from '@libs/logger';
 import { log } from '@libs/test-lib';
 
+const srcDirname = dirname(fileURLToPath(import.meta.url));
+
+const logger = Logger.create({ srcDirname, label: 'test-lib' });
+
 log('>>>', 'Example', 1);
+
+logger.warn('>>>', 'warn', 1, { a: () => {}, b: 1 }, [1, '2', {}, logger]);
+
+logger.error(new TypeError('type error', { cause: 'some cause' }));
