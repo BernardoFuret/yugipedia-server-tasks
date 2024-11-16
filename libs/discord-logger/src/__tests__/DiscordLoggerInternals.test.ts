ba263b0cbb772a6d0b2925b5d8df65baf657910c
BernardoFuret <bernardo.furet@hotmail.com>
2024-11-16 00:16:39 +0000

    discord-logger-tests: Update test suite for DiscordLoggerInternals to not rely on concrete classes

--- libs/discord-logger/src/__tests__/DiscordLoggerInternals.test.ts
+++ libs/discord-logger/src/__tests__/DiscordLoggerInternals.test.ts
@@ -1,15 +1,52 @@
-import { describe, expect, it, vi } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 
 import DiscordLoggerInternals from '../DiscordLoggerInternals';
-import InitialState from '../InitialState';
-import { type IDiscordLoggerState } from '../types.ts';
+import { type IDiscordClient, type IDiscordLoggerState } from '../types';
+
+const mockedDiscordClient = vi.fn();
+
+vi.mock(import('../DiscordClient'), () => ({
+  get default() {
+    return mockedDiscordClient;
+  },
+}));
+
+const mockedInitialState = vi.fn();
+
+vi.mock(import('../InitialState'), () => ({
+  get default() {
+    return mockedInitialState;
+  },
+}));
 
 const fakeLabel = 'fakeLabel';
 
 const fakeChannelId = 'fakeChannelId';
 
+const fakeDiscordClient: IDiscordClient = {
+  initiate: vi.fn(),
+  getChannelById: vi.fn(),
+  terminate: vi.fn(),
+};
+
+const fakeInitialState: IDiscordLoggerState = {
+  initiate: vi.fn(),
+  info: vi.fn(),
+  warn: vi.fn(),
+  error: vi.fn(),
+  terminate: vi.fn(),
+};
+
 describe('DiscordLoggerInternals', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   it('creates an instance with the expected members', () => {
+    mockedDiscordClient.mockImplementation(() => fakeDiscordClient);
+
+    mockedInitialState.mockImplementation(() => fakeInitialState);
+
     const discordLoggerInternals = new DiscordLoggerInternals({
       label: fakeLabel,
       channelId: fakeChannelId,
@@ -17,20 +54,22 @@ describe('DiscordLoggerInternals', () => {
 
     expect(discordLoggerInternals.getChannelId()).toBe(fakeChannelId);
 
-    expect(discordLoggerInternals.getDiscordClient()).toBeInstanceOf(DiscordLoggerInternals);
+    expect(discordLoggerInternals.getDiscordClient()).toBe(fakeDiscordClient);
 
     expect(discordLoggerInternals.getLabel()).toBe(fakeLabel);
 
-    expect(discordLoggerInternals.getState()).toBeInstanceOf(InitialState);
+    expect(discordLoggerInternals.getState()).toBe(fakeInitialState);
   });
 
   it('updates state', () => {
+    mockedInitialState.mockImplementation(() => fakeInitialState);
+
     const discordLoggerInternals = new DiscordLoggerInternals({
       label: fakeLabel,
       channelId: fakeChannelId,
     });
 
-    expect(discordLoggerInternals.getState()).toBeInstanceOf(InitialState);
+    expect(discordLoggerInternals.getState()).toBe(fakeInitialState);
 
     const newFakeState: IDiscordLoggerState = {
       initiate: vi.fn(),
